8d3357edda34ab07e202a89d0f6cc28f9d6e6c21
My Name <myEmail@example.com>
2020-10-05 12:06:17 +0300

    task2 done

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter number a:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number b:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number c:");
+            c = int.Parse(Console.ReadLine());
+
+            int sum = a + b + c;
+            double avg = sum / 3;
+
+            Console.WriteLine("Averege: {0}.", avg);
+        }
+    }
+}
