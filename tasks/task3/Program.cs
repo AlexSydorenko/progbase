7e11e587a347bacfc89c6fb1827f8495099cd13d
My Name <myEmail@example.com>
2020-10-05 21:51:34 +0300

    task3 done

---
+++ tasks/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your number:");
+            int num = int.Parse(Console.ReadLine());
+
+            int digit1 = num % 10;
+            int digit2 = (num / 10) % 10;
+            int digit3 = num / 100;
+
+            int sum = digit1 + digit2 + digit3;
+            Console.WriteLine("The sum is {0}.", sum);
+        }
+    }
+}
