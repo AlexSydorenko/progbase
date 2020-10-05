2d1f2d7a71c1d446655bee193280f269cc94c71d
My Name <myEmail@example.com>
2020-10-05 11:25:56 +0300

    task1 done

---
+++ tasks/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        //System.Console.WriteLine("HI!");
+        System.Console.WriteLine("Hello, my name is Sasha Sydorenko!");
+        WriteLine("I am from KP-03");
+        Write(1);
+        Write(3.14);
+        WriteLine("End.");
+    }
+}
